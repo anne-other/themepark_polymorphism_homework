8ae7b2ba7a9664c4dcacb5b44c8324b2a54216e6
anne-other <anne.c.ince@gmail.com>
2018-10-02 16:40:03 +0100

    Creating an abstract ride class

---
+++ src/main/java/rides/Ride.java
@@ -0,0 +1,32 @@
+package rides;
+
+public abstract class Ride {
+
+    private String name;
+    private double price;
+    private int minAge;
+    private double minHeight;
+
+    public void Ride(String name, double price, int minAge, double minHeight){
+        this.name = name;
+        this.price = price;
+        this.minAge = minAge;
+        this.minHeight = minHeight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getMinAge() {
+        return minAge;
+    }
+
+    public double getMinHeight() {
+        return minHeight;
+    }
+}
