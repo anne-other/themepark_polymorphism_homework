032f4b7935125b0ad21430262da910707389224b
anne-other <anne.c.ince@gmail.com>
2018-10-02 23:30:24 +0100

    Log flume has basic tests

---
+++ src/main/java/rides/LogFlume.java
@@ -0,0 +1,8 @@
+package rides;
+
+public class LogFlume extends Ride {
+
+    public LogFlume(String name, double price, int minAge, double minHeight) {
+        super(name, price, minAge, minHeight);
+    }
+}
---
+++ src/test/java/LogFlumeTest.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+import rides.LogFlume;
+
+import static org.junit.Assert.assertEquals;
+
+public class LogFlumeTest {
+
+    LogFlume logFlume;
+
+    @Before
+    public void setUp() throws Exception {
+        logFlume = new LogFlume("Splash Mountain", 9.99, 15, 1.7);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Splash Mountain", logFlume.getName());
+    }
+
+    @Test
+    public void hasPrice() {
+        assertEquals(9.99, logFlume.getPrice(), 0.01);
+    }
+
+    @Test
+    public void hasMinAge() {
+        assertEquals(15, logFlume.getMinAge());
+    }
+
+    @Test
+    public void hasMinHeight() {
+        assertEquals(1.7, logFlume.getMinHeight(), 0.1);
+    }
+}
